a10aff84be7f08356d2ab8d61637b6f994c32fe7
GuolongWang <da60511@gmail.com>
2025-07-06 10:51:30 +0800

    Transform H(ID) into NTT form

--- src/dilithium_py/ml_dsa/test.py
+++ src/dilithium_py/ml_dsa/test.py
@@ -1,34 +1,62 @@
-# Goal : Matrix * Matrix
 from dilithium_py.polynomials.polynomials_generic import PolynomialRing
 from dilithium_py.modules.modules_generic import Module
+from dilithium_py.ml_dsa import ML_DSA_44
 import random
 
 q = 8380417
 n = 256
 k = 4
+R = ML_DSA_44.R
+
 
 def BinaryToRing(binary):
-    R = PolynomialRing(q, n)
     coeffs = [0] * n
     coeffs[:len(binary)] = binary
-    return R(coeffs)
+    return R(coeffs).to_ntt()
 
-def OneOneMapping(h_value):
+def OneOneMapping(h_value): 
     chunk = n // (k ** 2)
-    return [BinaryToRing(h_value[i: i + chunk]) for i in range(0, n, chunk)]
+    
+    # k * k 條多項式
+    polys = [
+        BinaryToRing(h_value[i: i + chunk])
+        for i in range(0, n, chunk)
+    ]
+    
+    # Reshape 成 k-by-k matrix
+    matrix = [
+        polys[i * k: (i + 1) * k]
+        for i in range(k)
+    ]
 
+    M = Module(R)
+    return M(matrix)
+    
 def H(ID):
-    h_bytes = self._h(ID, 32) # 256 bits = 32 bytes
+    h_bytes = ML_DSA_44._h(ID, 32) # 256 bits = 32 bytes
     h_value = []
     for byte in h_bytes:      
         for bit in range(7, -1, -1):   
             h_value.append((byte >> bit) & 1)
         
-    return self.OneOneMapping(h_value)
+    return OneOneMapping(h_value)
 
-h_value = [random.randint(0, 1) for _ in range(256)]
-print(h_value)
-h = OneOneMapping(h_value)
-print(h)
+ID = [random.randint(0, 1) for _ in range(8)]
+M = H(bytes(ID))
 
-# Create a Polynomial Ring
+ID2 = [random.randint(0, 1) for _ in range(8)]
+M2 = H(bytes(ID2))
+
+print("H(ID) = ", M)
+print("H(ID2) = ", M2)
+#print(M @ M2)
+
+#test NTT
+R = ML_DSA_44.R
+f = R.random_element()
+print("f = ", f)
+print("NTT(f) = ", f.to_ntt())
+g = R.random_element()
+print("g = ", g)
+print("NTT(g) = ", g.to_ntt())
+print("fg = ", (f.to_ntt() * g.to_ntt()).from_ntt())
